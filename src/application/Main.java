e2e7ad2753d3aff5df7676e1b5513bbd1cdbfc7c
21724309 <21724309@N203-14.alumnos.uem.es>
2018-11-23 12:22:05 +0100

    Pantalla registro JFoenix

---
+++ src/application/Main.java
@@ -0,0 +1,31 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+
+
+public class Main extends Application {
+	
+	private AnchorPane mypane;
+	
+	@Override
+	public void start(Stage stage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("jfoenix.fxml"));
+			mypane = (AnchorPane) loader.load();
+			
+			stage.setTitle("Registrarse");
+			stage.setScene(new Scene(mypane));
+			stage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
